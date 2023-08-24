ab5bfa392aee3fdc2e249215aa012ba108cf4e6d
Dimas agung setyobudi <agungdimas390@gmail.com>
2023-08-24 10:43:51 +0700

    add auto size export

--- app/Exports/SaksiExport.php
+++ app/Exports/SaksiExport.php
@@ -6,30 +6,31 @@ use App\Models\Anggota;
 use App\Models\Saksi;
 use Illuminate\Contracts\View\View;
 use Maatwebsite\Excel\Concerns\FromView;
+use Maatwebsite\Excel\Concerns\ShouldAutoSize;
 
-class SaksiExport implements FromView
+class SaksiExport implements FromView, ShouldAutoSize
 {
     private $kecamatan;
     private $desa;
-    public function __construct(String $kecamatan = null,String $desa = null) {
+    public function __construct(String $kecamatan = null, String $desa = null)
+    {
         $this->kecamatan = $kecamatan;
         $this->desa = $desa;
     }
     public function view(): View
     {
-        if ($this->desa !=null  && $this->kecamatan !=null) {
+        if ($this->desa != null  && $this->kecamatan != null) {
             # code...
-            $saksi =Saksi::with('anggota')->where('desa', $this->desa)->where('kecamatan', $this->kecamatan)->orderBy('desa')->get();
-        }
-        else if ($this->desa ==null  && $this->kecamatan !=null) {
+            $saksi = Saksi::with('anggota')->where('desa', $this->desa)->where('kecamatan', $this->kecamatan)->orderBy('desa')->get();
+        } else if ($this->desa == null  && $this->kecamatan != null) {
             # code...
-            $saksi =Saksi::with('anggota')->where('kecamatan', $this->kecamatan)->orderBy('desa')->get();
-        }else{
+            $saksi = Saksi::with('anggota')->where('kecamatan', $this->kecamatan)->orderBy('desa')->get();
+        } else {
 
             $saksi = Saksi::with('anggota')->orderBy('desa')->get();
         }
         return view('admin.export.saksi', [
-            'saksi' =>$saksi
+            'saksi' => $saksi
         ]);
     }
 }
